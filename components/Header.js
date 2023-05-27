1bf426b1aa65e1a3b4b7830b17a33d40e21edebb
almogzur <almogzur1@gmail.com>
2023-05-27 23:37:00 +0300

    width 7 inherit

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import Link from 'next/link';
 const NavMenu = () => {
   return (
-   <div style={{width:"100vw",background:"#e8e9e4", height:"70px"}}>
+   <div style={{width:"inherit",background:"#e8e9e4", height:"70px"}}>
       <Link href={"/"}>Home</Link>
       <Link href={"/reviews"}>Reviews</Link>
       <Link href={"/about"}>About</Link>
