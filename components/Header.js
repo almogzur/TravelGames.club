7cbd718e44d3dd5ba29c9d55cd8dc97069fd9212
almogzur <almogzur1@gmail.com>
2023-05-28 10:04:30 +0300

    width 7 copy footer and changes

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import * as Separator from '@radix-ui/react-separator';
 import Link from 'next/link';
+
 const NavMenu = () => {
   return (
-   <div style={{width:"100%",background:"#e8e9e4", height:"70px" ,display:"flex", flexGrow:"4",}}>
-      <Link href={"/"}>Home</Link>
-      <Link href={"/reviews"}>Reviews</Link>
-      <Link href={"/about"}>About</Link>
 
+  <div style={{  
+            position: "absolute",
+            padding: "10px" ,
+            top: "0px",
+            width: "100%",
+            zIndex:"3",
+            display: 'flex',
+            flexDirection:"column",
+            justifyContent:"center",
+            flexGrow:"4",
+            background:"#e8e9e4",
+            margin:"20px",
+
+        }}>
+    <div style={{ display: 'flex', height: 20, alignItems: 'center'  ,justifyContent:"center" }}>
+
+     <Link  href={"/"} className="Text">Home</Link>
+
+      <Separator.Root
+        className="SeparatorRoot"
+        decorative
+        orientation="vertical"
+        style={{ margin: '0 15px' }}
+      />
+      <Link  href={"/reviews"} className="Text">Reviews</Link>
+      
+      <Separator.Root
+        className="SeparatorRoot"
+        decorative
+        orientation="vertical"
+        style={{ margin: '0 15px' }}
+      />
+      <Link  href={"/about"} className="Text">About</Link>
     </div>
+  </div>
   );
 };
 
