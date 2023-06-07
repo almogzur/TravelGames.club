3aa870cdfff243de43b4236ee909c7733ef6cbef
almogzur <almogzur1@gmail.com>
2023-06-07 12:15:53 +0300

    new min width test 375 ( lowes ther is )

--- components/ImageLayout.js
+++ components/ImageLayout.js
@@ -1,40 +1,16 @@
-'use client'
-import  {motion, AnimatePresence} from 'framer-motion'
-import { useState } from 'react'
-import CircumIcon from "@klarr-agency/circum-icons-react"
 
-
-const variants = {
-  enter: () => {
-    return {
-      opacity: 0
-    };
-  },
-  center: {
-    opacity: 1
-  },
-  exit: () => {
-    return {
-      opacity: 0
-    };
-  }
-};
-
+import Image from "next/image"
 export default function ImageLayout ({mainPhoto}){
 return (
-       <div tabIndex={1} className='gallerywrapeer' >
-          <motion.img 
+       <div  className='gallerywrapeer' >
+          <Image
+             alt=""
+             height={400}
+             width={400}
              loading='lazy'
-             height={500}
-             width={500}
              src={mainPhoto}
-             variants={variants}
-             initial="enter"
-             animate="center"
-             exit="exit"
              transition={{ opacity: { duration: 1 }}}
            />   
-        <div className='photoText' >Press On The Photo </div>
        </div>
      
 
