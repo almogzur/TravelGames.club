4dc8721972be2dc4f9694f0ad8bc5f7f92b79fea
almogzur <almogzur1@gmail.com>
2024-01-10 11:55:28 +0200

    type to local storeg functions

--- util/localstoreg.tsx
+++ util/localstoreg.tsx
@@ -1,13 +1,13 @@
 
 
 // config LS for use in hook 
-export const setLocalStorageItem = (key, value) => {
+export const setLocalStorageItem = (key: any, value: string) => {
     if (typeof window !== 'undefined') {
       localStorage.setItem(key, value);
     }
   };
   
-export const getLocalStorageItem = (key) => {
+export const getLocalStorageItem = (key: any) => {
     if (typeof window !== 'undefined') {
       const item = localStorage.getItem(key);
       return item ? item : null;
