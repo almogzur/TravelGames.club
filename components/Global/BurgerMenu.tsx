8801df6222bfade54c742eddb2f2eff7247863eb
almogzur <almogzur1@gmail.com>
2024-01-14 11:13:40 +0200

    li on mouse backround

--- components/Global/BurgerMenu.tsx
+++ components/Global/BurgerMenu.tsx
@@ -4,46 +4,64 @@ import Dropdown from 'rc-dropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import 'rc-dropdown/assets/index.css';
+import { useState } from 'react';
 
 
- function onSelect({ key }:any) {
-    console.log(`${key} selected`);
-  }
-  
- function onVisibleChange(visible: any) {
-    console.log(visible);
-  }
-
- const itemStyle : React.CSSProperties = {
-    color:"red",
-    width:"300px",
-    height:"50px",
-    marginTop:"20px",
-    display:"flex",
-    justifyContent:"center",
-    background:""
-}
-
-  const InnerMenu = (
-        
-    <Menu onSelect={onSelect}>
-      <MenuItem onMouseOver={()=>{ }}
-       style={itemStyle} key="1">Reviews</MenuItem>
-      <Divider />
-      <MenuItem  style={itemStyle}key="2">About</MenuItem>
-    </Menu>
-    
-  );
-  
   const BurgerMenu = ()=>{
+    let MenuItemsArray = ["Reviews","About"]
+
+
+    
+    const itemCss :React.CSSProperties= {
+      color:"red",
+      width:"300px",
+      height:"50px",
+      marginTop:"20px",
+      display:"flex",
+      justifyContent:"center",
+  
+
+    }
+    const handleMouseOver = (e:any) => {
+      e.target.style.background="#3f71ae"
+
+    };
+  
+    const handleMouseOut = (e:any) => {
+      e.target.style.background="#fff"
+
+
+    };
+
+    const ComMenu = ():any => {
+
+      return<Menu>
+        {MenuItemsArray.map((item,i)=>{
+          return(<MenuItem 
+          style={itemCss} 
+          key={i}
+          onMouseOver={(e)=>handleMouseOver(e)}
+          onMouseOut={(e)=>handleMouseOut(e)}
+          >
+          {item}
+          <Divider/>
+          </MenuItem>)
+        })}
+      </Menu>
+
+
+    }
+
+   
+  
     
     return(
       <div style={{height:"70px", background:"#3f71ae",display:"flex",justifyContent:"flex-end"}}>
         <Dropdown
           trigger={['click']}
-          overlay={InnerMenu}
+          overlay={ComMenu}
           animation="slide-up"
-          onVisibleChange={onVisibleChange}
+     
         >
           <button style={{width:"70px" }}>{<FontAwesomeIcon size={"2x"} icon={faBars} />}</button>
         </Dropdown>
